b660771576132c27cda55f12b08c81f40c519ca7
Bruno Vitte <bruno_vitte@hotmail.com>
2024-05-20 20:28:26 -0600

    yes

--- src/Client.py
+++ src/Client.py
@@ -19,35 +19,30 @@ dpg_window = dpg.generate_uuid()
 def print_me(sender):
 	print(f"Menu Item: {sender}")
 
-def test_connection(sender, isVisible):
-	isVisible = not isVisible
+def test_connection(email):
+	print("Aqui si jala")
+	print(email)
 
-def change_window():
+def change_window(one):
 	dpg.delete_item(dpg_window, children_only=True) # clean the window from the past interface
+	print(one)
 	render_second_window(parent=dpg_window)
 
 def render_second_window(parent=0):
-	with dpg.group(horizontal=True, parent=parent) as group:
+	with dpg.group(horizontal=False, parent=parent) as group:
+		email = dpg.add_input_text(label="Correo")
 		dpg.add_text("Hello, world", parent=group)
-		dpg.add_button(label="Save", callback=test_connection, parent=group)
+		dpg.add_button(label="Save", callback=test_connection(email), parent=group)
 		dpg.add_input_text(label="string", default_value="Quick brown fox", parent=group)
 		dpg.add_slider_float(label="float", default_value=0.273, max_value=1, parent=group)
 
 def render_login(parent = 0):
 	with dpg.group(horizontal=False, parent=parent) as group:
-		with dpg.menu(label="File"):
-			dpg.add_menu_item(label="Save", callback=print_me)
-			dpg.add_menu_item(label="Save As", callback=print_me)
-
-		with dpg.menu(label="Settings"):
-			dpg.add_menu_item(label="Setting 1", callback=print_me, check=True)
-			dpg.add_menu_item(label="Setting 2", callback=print_me)
-
-		dpg.add_menu_item(label="Help", callback=print_me)
+		email = dpg.add_input_text(label="Correo")
 
 		with dpg.menu(label="Widget Items"):
 			dpg.add_checkbox(label="Pick Me", callback=print_me)
-			dpg.add_button(label="Press Me", callback=change_window)
+			dpg.add_button(label="Press Me", callback=change_window(email))
 			dpg.add_color_picker(label="Color Me", callback=print_me)
 
 with dpg.window(
